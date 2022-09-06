c0e0ce50487e85f94c17d26e162987ac54c8098d
TrinityWeaver <sebastian.marynicz@gmail.com>
2022-09-07 00:11:57 +0100

    colored, writings and fully done s3. SQS started

--- setup.py
+++ setup.py
@@ -2,15 +2,12 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='helper',
-    version='0.1',
+    name="helper",
+    version="0.1",
     packages=find_packages(),
-    install_requires=[
-        'click',
-        'boto3',
-    ],
-    entry_points='''
+    install_requires=["click", "boto3", "colored"],
+    entry_points="""
         [console_scripts]
-        helper=helpercli:main
-        '''
+        helper=helpercli:helper
+        """,
 )
