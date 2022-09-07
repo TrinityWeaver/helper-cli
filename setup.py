cfffa3488283f8efb4f4d9088424cf16a188fb95
TrinityWeaver <sebastian.marynicz@gmail.com>
2022-09-07 23:21:57 +0100

    PIP packagage

--- setup.py
+++ setup.py
@@ -1,13 +1,20 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 
 setup(
     name="helper",
-    version="0.1",
+    version="1.0.1",
+    author="Sebastian Marynicz",
+    author_email="sebastian.marynicz@gmail.com",
+    description="Helper CLI for quick removal of AWS resources.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/TrinityWeaver/helper-cli",
+    license="MIT",
     packages=find_packages(),
     install_requires=["click", "boto3", "colored"],
-    entry_points="""
-        [console_scripts]
-        helper=helpercli:helper
-        """,
+    entry_points={"console_scripts": ["helper=helper.helpercli:helper"]},
 )
